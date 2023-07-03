9705f08d52c861ff38b86914ada6ad5dcec665c1
李瑞鑫 <15939942017@163.com>
2023-07-03 18:51:01 +0800

    video-to-canvas2.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { CSSProperties, useCallback, useEffect, useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import video1 from "./assets/video1.mp4";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Player, { Events } from "xgplayer";
-import HlsPlugin from "xgplayer-hls";
 import "xgplayer/dist/index.min.css";
-// let canvas,
-//   ctx,
-//   video,
-//   endTime = 4,
-//   requestAnimationFramesId,
 const w = window;
 const requestAnimationFrames = w.requestAnimationFrame;
-const canvasBoxScale = 2;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let requestAnimationFramesId: number;
 function App() {
   const { PLAY } = Events;
@@ -25,20 +17,11 @@ function App() {
   const [styles, setStyles] = useState<CSSProperties>({});
   const ratio = useRef<number>(1);
 
-  /**
-   * Checks if two RGB colors are similar.
-   *
-   * @param rgb1 The first RGB color to compare.
-   * @param rgb2 The second RGB color to compare.
-   * @param tolerance The tolerance for similarity. A higher value means more tolerance.
-   * @returns True if the colors are similar, false otherwise.
-   */
   interface RGBColor {
     r: number;
     g: number;
     b: number;
   }
-
   const areColorsSimilar = useCallback(
     (rgb1: RGBColor, rgb2: RGBColor, tolerance: number): boolean => {
       const rDiff = Math.abs(rgb1.r - rgb2.r);
@@ -51,15 +34,11 @@ function App() {
 
   const switchToCanvas = useCallback(() => {
     if (player.current?.paused || player.current?.ended) return;
-    console.log(1111);
-
     const canvas = canvasRef.current as HTMLCanvasElement;
     const video = videoWrapperRef.current?.querySelector(
       "video"
     ) as HTMLVideoElement;
-
     ctx.current.drawImage(video, 0, 0, canvas.width, canvas.height);
-
     const image = ctx.current.getImageData(0, 0, canvas.width, canvas.height),
       imageData = image.data;
     for (let i = 3, len = imageData.length; i < len; i = i + 4) {
@@ -81,9 +60,7 @@ function App() {
         imageData[i] = 0;
       }
     }
-
     ctx.current.putImageData(image, 0, 0, 0, 0, canvas.width, canvas.height);
-
     requestAnimationFramesId = requestAnimationFrames(switchToCanvas);
   }, [areColorsSimilar]);
 
@@ -128,20 +105,8 @@ function App() {
   );
   useEffect(() => {
     initVideo(play);
-    //   const player = new Player({
-    //     id: "video1",
-    //     // url: video1,
-    //     url: "http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8",
-    //     height: "auto",
-    //     width: "100%",
-    //     autoplay: true,
-    //     autoplayMuted: true,
-    //     controls: false,
-    //     plugins: [HlsPlugin],
-    //   });
   }, [initVideo, play]);
 
-  // 计算当前屏幕是几倍屏
   const getPixelRatio = (context: any) => {
     const backingStore: any =
       context.backingStorePixelRatio ||
@@ -153,7 +118,6 @@ function App() {
       1;
     return (window.devicePixelRatio || 1) / backingStore;
   };
-  console.log("styles", styles);
 
   return (
     <div>
